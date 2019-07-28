d69eba66b17d25a8feba715c885592f65836cbbf
Korea-subinpark <subinpark.re@gmail.com>
2019-07-28 12:03:31 +0900

    입력값 이외 BadRequest 처리
    입력값 이외 BadRequest 처리

--- rest-api-with-spring/src/test/java/com/example/demo/events/EventControllerTests.java
+++ rest-api-with-spring/src/test/java/com/example/demo/events/EventControllerTests.java
@@ -39,6 +39,38 @@ public class EventControllerTests {
 	
 	@Test
 	public void createEvent() throws Exception {
+		EventDto event = EventDto.builder()
+				.name("Spring")
+				.description("REST API Development with Spring")
+				.beginEnrollmentDateTime(LocalDateTime.of(2019, 07, 27, 23, 44))
+				.closeEnrollmentDateTime(LocalDateTime.of(2019, 07, 27, 23, 44))
+				.beginEventDateTime(LocalDateTime.of(2019, 07, 27, 23, 44))
+				.endEventDateTime(LocalDateTime.of(2019, 07, 27, 23, 44))
+				.basePrice(100)
+				.maxPrice(200)
+				.limitOfEnrollment(100)
+				.location("Seoul")
+				.build();
+		
+		mockMvc.perform(post("/api/events/")
+					.contentType(MediaType.APPLICATION_JSON_UTF8)//요청 타입
+					.accept(MediaTypes.HAL_JSON)//어떤 응답을 원하는지
+					.content(objectMapper.writeValueAsString(event))
+				)
+				.andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("id").exists())
+				.andExpect(header().exists(HttpHeaders.LOCATION))
+				.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+				.andExpect(jsonPath("id").value(Matchers.not(100)))//id와 free는 입력되면 안되는 값들이기 때문에 없어야 한다
+				.andExpect(jsonPath("free").value(Matchers.not(true)))
+				.andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT))
+			;
+	}
+	
+	
+	@Test
+	public void createEventBadRequest() throws Exception {
 		Event event = Event.builder()
 				.id(100)
 				.name("Spring")
@@ -57,19 +89,11 @@ public class EventControllerTests {
 				.build();
 		
 		mockMvc.perform(post("/api/events/")
-					.contentType(MediaType.APPLICATION_JSON_UTF8)//요청 타입
-					.accept(MediaTypes.HAL_JSON)//어떤 응답을 원하는지
-					.content(objectMapper.writeValueAsString(event))
-				)
+				.contentType(MediaType.APPLICATION_JSON_UTF8)//요청 타입
+				.accept(MediaTypes.HAL_JSON)//어떤 응답을 원하는지
+				.content(objectMapper.writeValueAsString(event)))
 				.andDo(print())
-				.andExpect(status().isCreated())
-				.andExpect(jsonPath("id").exists())
-				.andExpect(header().exists(HttpHeaders.LOCATION))
-				.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-				.andExpect(jsonPath("id").value(Matchers.not(100)))//id와 free는 입력되면 안되는 값들이기 때문에 없어야 한다
-				.andExpect(jsonPath("free").value(Matchers.not(true)))
-				.andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT))
-				
+				.andExpect(status().isBadRequest())
 		;
 	}
 	
